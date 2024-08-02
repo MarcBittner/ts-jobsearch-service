16d04d5ddbae2ae4da4a5c8424ab4734e2939f3d
Marc Bittner <marc.bittner@ring.com>
2024-08-02 15:33:31 -0700

    [WIP] Add Build Logic
    - Add build logic, not in working condition
    
    - Push to preserve state of the import functons before refactoring them
    
     Please enter the commit message for your changes. Lines starting
     with '' will be ignored, and an empty message aborts the commit.
    
    --
    
     On branch main
     Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	renamed:    src/.gitignore -> .gitignore
    	new file:   build.bashlog
    	new file:   build.functions
    	new file:   build.sh
    	new file:   build.variables
    	modified:   env
    	new file:   log.sh
    	new file:   package-lock.json
    	new file:   package.json
    	deleted:    src/env
    	new file:   test.sh
    	new file:   tsconfig.json

---
+++ build.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+unset me executionDirectory sourceDirectory runtime importFiles binaryRequirements tapRequirements
+set -a
+runtime="$(date +%y%m%d_%H%M%S)"
+me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+executionDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+sourceDirectory="$( cd -- "$(dirname -- "$(readlink -- "${BASH_SOURCE[0]}")")" >/dev/null 2>&1 && pwd )"
+
+
+#SYSLOG_FACILITY='local7'
+LOG_TAG='ts-jobsearch-service-build' # Optional syslog app name
+LOG_LEVEL='DEBUG'
+DATETIME_FORMAT="+%Y-%m-%dT%H:%M:%S%z" # "+%FT%T%Z"
+#LOG_FILE="${logFileName-"${me}.$(date +%FT%T%Z).log"}"
+LOG_FILE="${logFileName-"${me}.log"}"
+
+  # Define imports
+
+importFiles=(
+            "${variablesFile-"build.variables"}"
+            "${functionsFile-"build.functions"}"
+            "${bashlogFile-"build.bashlog"}"
+            )
+
+  # Define nonce import function
+
+importOrAbort() {
+  local importFile debug
+
+  while [[ ${1} ]]; do
+      case "${1}" in
+        --file)
+          importFile=${2}
+          shift
+          ;;
+        --debug)
+          debugOutput=1
+          ;;
+        *)
+          echo "Unknown parameter: ${1}" >&2
+          return 1
+          ;;
+      esac
+
+      if ! shift; then
+        echo 'Missing parameter argument.' >&2
+        return 1
+      fi
+    done
+
+  if [ -r ${importFile} ]; then
+      . ${importFile}
+      [[ debugOutput -gt 0 ]] &&
+      echo "Successfully sourced ${importFile}"
+  else
+      [[ debugOutput -gt 0 ]] &&
+      echo "failed to import ${importFile}: aborting"
+      exit 1
+  fi
+}
+
+# Loop through files to source
+
+for import in "${importFiles[@]}"
+  do
+    [[ debugOutput -gt 0 ]] && echo "Sourcing ${sourceDirectory}/${import}"
+    importOrAbort --file ${sourceDirectory}/${import}
+  done
+set +a
+
+  # Optionally set -x with a friendly variable
+  importModules=(
+              "dotenv"
+              "node-schedule"
+              "axios"
+              "csv-writer"
+              "ffs"
+              )
+
+
+# . ./build.bashlog
+[[ bash_debug -gt 0 ]] && set -x
+
+
+
+echo $LOG_FILE
+_bashlog info "HI"
+#rm -f ./src/*.js
+_bashlog info < <($(npm install  ${importModules[@]}) 2>&1)
